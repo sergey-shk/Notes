f3e380629d536a8ad36e1fd9ce4cd43fddf660c7
sergey-shk <67577294+sergey-shk@users.noreply.github.com>
2020-07-15 18:23:31 +0700

    1

--- Notes/note/models.py
+++ Notes/note/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=60, default='Untitled',db_index=True)
+    body = models.TextField(max_length=400, default='...')
+    pub_date = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(auto_now_add=False, default=None)
+
+    def __str__(self):
+        return '{}'.format(self.title)
